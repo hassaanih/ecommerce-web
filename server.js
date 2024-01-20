ac4ac8ca35f56d3482753f9cb0f3ac8c4661a165
MuzzamilAKhan <muzzamil.khan@synergates.com>
2024-01-21 03:08:45 +0500

    minor check

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors());
 
+// setup
 mongoose.connect('mongodb://localhost:27017/ecommerce', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
